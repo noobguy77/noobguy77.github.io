72f549d61205188d69524332d49e6959127bebfd
noobguy77 <impmailonly7@gmail.com>
2023-08-13 16:59:36 +0530

    Updated Resume

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,8 @@
 const BLACKLISTED_KEY_CODES = [38];
 const COMMANDS = {
-    commands: 'Supported commands: <span class="code">about</span>, <span class="code">skills</span>, <span class="code">education</span>, <span class="code">resume</span>, <span class="code">contact</span>, <span class="code">coding_profiles</span>.',
+    commands: 'Supported commands: <span class="code">about</span>,<span class="code">experience</span>, <span class="code">skills</span>, <span class="code">education</span>, <span class="code">resume</span>, <span class="code">contact</span>, <span class="code">coding_profiles</span>.',
     about: "Hey! <br>I'm Jayanth Naidu. I'm a CS Undergrad '23 who has a knack for coding.",
+    experience: "Currenly working as a Product Engineer at <a href='https://lji.io' class='success link '>Loyalty Juggernaut</a>",
     skills: '<span class="code">Languages:</span> C/C++, Python3, Java, JavaScript.<br><span class="code">Frameworks:</span> SpringBoot, Express.js<br><span class="code">Databases:</span> MongoDB, MySQL.<br><span class="code">Tools:</span> Bash, Git & GitHub, Windows, Linux.<br><span class="code"> AWS Services:</span> EC2, Lambda, DynamoDB, S3 and Redshift',
     education: "Bachelor of Technology — Computer Science, CGPA - 8.50/10 <br>IARE Affiliated to JNTU Hyderabad.",
     contact : "Number - (+91)9493269995, Email - impmailonly7@gmail.com",
